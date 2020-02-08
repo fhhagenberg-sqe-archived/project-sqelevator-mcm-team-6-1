a4e42e861ae3091d425d2bc8dbdef4e879cb32ee
Manuel Leibetseder <leibetseder.manuel@fh-hagenberg.at>
2020-02-08 14:54:37 +0100

    extract a controllabel

--- src/main/java/at/fhhagenberg/sqelevator/view/controls/ControlLabel.java
+++ src/main/java/at/fhhagenberg/sqelevator/view/controls/ControlLabel.java
@@ -1,4 +1,19 @@
 package at.fhhagenberg.sqelevator.view.controls;
 
-public class ControlLabel {
+import at.fhhagenberg.sqelevator.BorderStyle;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+
+public class ControlLabel extends Label {
+
+    public ControlLabel(String text) {
+        this.setText(text);
+        this.setAlignment(Pos.CENTER);
+        this.setBorder(BorderStyle.THIN_BLACK.value());
+        this.setStyle("-fx-font-size: 20;");
+        this.setPrefWidth(120.0);
+        this.setPadding(new Insets(20, 0, 20, 0));
+    }
 }
